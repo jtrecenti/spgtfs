c54d332600c7b59b83c534aca75f6cd94aa1ccfb
Julio Trecenti <julio.trecenti@gmail.com>
2017-02-17 11:12:50 -0200

    gtfs docs

---
+++ R/gtfs.R
@@ -0,0 +1,11 @@
+#' GTFS-SP data
+#'
+#' GTFS-SP data download on 2017-02-17.
+#'
+#' @format A data frame with 10 rows and 2 variables:
+#' \describe{
+#'    \item{arq}{file name}
+#'    \item{data}{list column with a tibble inside each element}
+#' }
+#' @source \url{https://www.cnj.jus.br/improbidade_adm/consultar_requerido.php}
+"gtfs"
