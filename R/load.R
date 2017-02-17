98e5813896513e20df54aed836045ac045f65d80
Julio Trecenti <julio.trecenti@gmail.com>
2017-02-17 11:13:04 -0200

    force utf8

--- R/load.R
+++ R/load.R
@@ -15,9 +15,11 @@ spgtfs_load <- function(zipfile = 'data-raw/gtfs/gtfs.zip') {
     tibble::enframe() %>%
     dplyr::select(arq = value) %>%
     dplyr::group_by(arq) %>%
-    dplyr::do(data = tibble::as_tibble(
-      data.table::fread(sprintf('%s/%s', d, .$arq))
-    )) %>%
+    dplyr::do(data = {
+      sprintf('%s/%s', d, .$arq) %>%
+        data.table::fread(encoding = 'UTF-8') %>%
+        tibble::as_tibble()
+      }) %>%
     dplyr::ungroup() %>%
     tibble::remove_rownames()
   file.remove(sprintf('%s/%s', d, arqs))
